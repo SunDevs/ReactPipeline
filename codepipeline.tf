4567ba872a2419a00448b2cd49c202c92686b5b0
Ivan Botero <ivan.botero@sundevs.com>
2021-03-15 16:24:59 -0500

    Created pipeline

---
+++ codepipeline.tf
@@ -0,0 +1,55 @@
+resource "aws_codepipeline" "pipeline" {
+  name     = "pipeline-${lower(var.PROJECT_NAME)}-${random_pet.value.id}"
+  role_arn = aws_iam_role.codepipeline.arn
+  artifact_store {
+    type     = "S3"
+    location = aws_s3_bucket.artifact.id
+  }
+  stage {
+    name = "Source"
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      input_artifacts  = []
+      output_artifacts = ["SOURCE_OUTPUT"]
+      configuration = {
+        ConnectionArn    = var.CODESTAR_CONNECTION_ARN
+        FullRepositoryId = var.REPOSITORY_NAME
+        BranchName       = var.REPOSITORY_BRANCH
+      }
+    }
+  }
+  stage {
+    name = "Start"
+    action {
+      name             = "Start"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["SOURCE_OUTPUT"]
+      output_artifacts = ["START_OUTPUT"]
+      version          = "1"
+      configuration = {
+        ProjectName = aws_codebuild_project.start.id
+      }
+    }
+  }
+  stage {
+    name = "Stop"
+    action {
+      name             = "Stop"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      input_artifacts  = ["START_OUTPUT"]
+      output_artifacts = ["STOP_OUTPUT"]
+      version          = "1"
+      configuration = {
+        ProjectName = aws_codebuild_project.stop.id
+      }
+    }
+  }
+}
