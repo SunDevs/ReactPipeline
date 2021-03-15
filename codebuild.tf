8dcf2fa7d3c695f349bd7b8392e4bd4e118ad98f
Ivan Botero <ivan.botero@sundevs.com>
2021-03-15 14:43:56 -0500

    Correction in EC2 ID

--- codebuild.tf
+++ codebuild.tf
@@ -10,9 +10,11 @@ resource "aws_codebuild_project" "start" {
   service_role   = aws_iam_role.codebuild.arn
   source_version = var.BITBUCKET_BRANCH
   source {
-    type      = "BITBUCKET"
-    location  = var.BITBUCKET_REPOSITORY
-    buildspec = templatefile("${path.module}/buildspec/start.yml", { INSTANCE_ID = aws_instance.ec2.arn })
+    type     = "BITBUCKET"
+    location = var.BITBUCKET_REPOSITORY
+    buildspec = templatefile("${path.module}/buildspec/start.yml", {
+      INSTANCE_ID = basename(aws_instance.ec2.arn)
+    })
   }
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
@@ -36,8 +38,10 @@ resource "aws_codebuild_project" "stop" {
   name         = "codebuild-stop-${lower(var.PROJECT_NAME)}-${random_pet.value.id}"
   service_role = aws_iam_role.codebuild.arn
   source {
-    type      = "NO_SOURCE"
-    buildspec = templatefile("${path.module}/buildspec/stop.yml", { INSTANCE_ID = aws_instance.ec2.arn })
+    type = "NO_SOURCE"
+    buildspec = templatefile("${path.module}/buildspec/stop.yml", {
+      INSTANCE_ID = basename(aws_instance.ec2.arn)
+    })
   }
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
