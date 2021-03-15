ea005ee160f69c0b6927700a7cb69b5cd41dcd2d
Ivan Botero <ivan.botero@sundevs.com>
2021-03-15 15:31:06 -0500

    Change related to s3 migration

--- codebuild.tf
+++ codebuild.tf
@@ -1,10 +1,8 @@
 resource "aws_codebuild_project" "start" {
-  name           = "codebuild-start-${lower(var.PROJECT_NAME)}-${random_pet.value.id}"
-  service_role   = aws_iam_role.codebuild.arn
-  source_version = var.BITBUCKET_BRANCH
+  name         = "codebuild-start-${lower(var.PROJECT_NAME)}-${random_pet.value.id}"
+  service_role = aws_iam_role.codebuild.arn
   source {
-    type     = "BITBUCKET"
-    location = var.BITBUCKET_REPOSITORY
+    type = "NO_SOURCE"
     buildspec = templatefile("${path.module}/buildspec/start.yml", {
       INSTANCE_ID = basename(aws_instance.ec2.arn)
     })
@@ -42,7 +40,10 @@ resource "aws_codebuild_project" "stop" {
     type         = "LINUX_CONTAINER"
   }
   artifacts {
-    type = "NO_ARTIFACTS"
+    type      = "S3"
+    packaging = "ZIP"
+    name      = "STOP_OUTPUT"
+    location  = aws_s3_bucket.artifact.id
   }
   logs_config {
     cloudwatch_logs {
