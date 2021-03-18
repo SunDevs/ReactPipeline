8504f367d6a72d5a138e2ffc64d0a5c3f07dcc16
Ivan Botero <ivan.botero@sundevs.com>
2021-03-18 11:33:39 -0500

    Fix script.ps1

--- codedeploy/script.ps1
+++ codedeploy/script.ps1
@@ -1,6 +1,20 @@
+#
+# Get into the caller script folder
+#
 cd $PSScriptRoot
 
-Invoke-Expression "& 'C:\Program Files\Git\usr\bin\ls.exe'"
-Invoke-Expression "& 'C:\Program Files\nodejs\npm.cmd' install"
-Invoke-Expression "& 'C:\Users\Administrator\AppData\Roaming\npm\yarn.cmd'"
-Invoke-Expression "& 'C:\Users\Administrator\AppData\Roaming\npm\yarn.cmd' package"
+#
+# Set the PATH environment variable
+#
+$env:Path = "C:\Python39\Scripts\;C:\Python39\;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Windows\System32\OpenSSH\;C:\Program Files\Amazon\cfn-bootstrap\;C:\Program Files\nodejs\;C:\ProgramData\chocolatey\bin;C:\Program Files\Git\cmd;C:\Users\Administrator\AppData\Local\Microsoft\WindowsApps;C:\Users\Administrator\AppData\Roaming\npm;C:\Program Files\Git\usr\bin;C:\Program Files\Git\mingw64\libexec\git-core;"; 
+
+#
+# Install dependencies
+#
+npm install
+yarn
+
+#
+# Package the source
+#
+yarn package
