3c962830ff52c3ae9575b421dc1554955d0e037b
Ivan Botero <ivan.botero@sundevs.com>
2021-03-18 10:26:40 -0500

    Changes in execution

--- codedeploy/script.ps1
+++ codedeploy/script.ps1
@@ -1,5 +1,6 @@
 cd $PSScriptRoot
 
-npm install 
-yarn
-yarn package
+Invoke-Expression "& 'C:\Program Files\Git\usr\bin\ls.exe'"
+Invoke-Expression "& 'C:\Program Files\nodejs\npm.cmd' install"
+Invoke-Expression "& 'C:\Users\Administrator\AppData\Roaming\npm\yarn.cmd'"
+Invoke-Expression "& 'C:\Users\Administrator\AppData\Roaming\npm\yarn.cmd' package"
