8be9ee94cad95682e70dc26aedf5cb05cd78d779
Ivan Botero <ivan.botero@sundevs.com>
2021-04-09 11:17:46 -0500

    Added ec2 module and eip

--- ec2.tf
+++ ec2.tf
@@ -28,7 +28,11 @@ resource "aws_key_pair" "ec2" {
   public_key = var.EC2_PUBLIC_KEY
 }
 
-resource "aws_instance" "ec2" {
+module "ec2" {
+  source                      = "terraform-aws-modules/ec2-instance/aws"
+  version                     = "2.17.0"
+  name                        = "ec2"
+  instance_count              = 1
   ami                         = data.aws_ami.windows.id
   instance_type               = var.EC2_INSTANCE_TYPE
   subnet_id                   = data.aws_subnet.subnet.id
@@ -37,11 +41,24 @@ resource "aws_instance" "ec2" {
   disable_api_termination     = false
   vpc_security_group_ids      = [aws_security_group.ec2.id]
   key_name                    = aws_key_pair.ec2.key_name
-  root_block_device {
-    volume_type = "gp2"
-    volume_size = var.EC2_VOLUME_SIZE
-  }
+  root_block_device = [
+    {
+      volume_type = "gp2"
+      volume_size = var.EC2_VOLUME_SIZE
+    }
+  ]
   tags = {
-    "Name" = "ec2-${lower(var.PROJECT_NAME)}-${random_pet.value.id}"
+    Name = "ec2-${lower(var.PROJECT_NAME)}-${random_pet.value.id}"
+  }
+  volume_tags = {
+    Name = "ec2-${lower(var.PROJECT_NAME)}-${random_pet.value.id}"
+  }
+}
+
+resource "aws_eip" "eip" {
+  vpc      = true
+  instance = module.ec2.id[0]
+  tags = {
+    Name = "ec2-${lower(var.PROJECT_NAME)}-${random_pet.value.id}"
   }
 }
